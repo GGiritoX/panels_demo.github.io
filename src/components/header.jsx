e2f821cf7931a4b961f70e7c2f19478f7904bccb
Vit <ggiritox@gmail.com>
2024-02-27 21:27:59 +0200

    fix bw styles and menu button overlap on devices

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,11 +6,13 @@ export default function Header() {
     return (
         <>
             <header className="fade-2">
-                <div className="logoButton">
-                    <a className="text-decoration-none" href="/">Site Logo</a>
-                </div>
-                <div className="sideMenuButton">
-                    <SideMenu/>
+                <div>
+                    <div className="position-absolute top-50 start-50 translate-middle logoButton">
+                        <a className="text-decoration-none" href="/">SiteLogo</a>
+                    </div>
+                    <div className="position-absolute end-0 translate-middle sideMenuButton">
+                        <SideMenu/>
+                    </div>
                 </div>
             </header>
         </>
