62defe2c122631cf8ed84f5d07eb2fb82a877624
Vit <ggiritox@gmail.com>
2024-04-07 16:47:14 +0300

    edit contatcs, services pages. Edit footer icons

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,5 +1,6 @@
-import Header from "../components/header-project"
+import Header from "../components/header-main"
 import Footer from "../components/footer"
+import '../css/services.css'
 
 
 export default function Services() {
@@ -8,11 +9,13 @@ export default function Services() {
             <Header />
             <main>
 
-                <div className="text">
-                    Послуги:<br />
-                    - Проєктування<br />
-                    - Візуалізація для девелопмента<br />
-                    - Створення концепції<br />
+                <div className="stext fade-2">
+                    <div>Створення концепцій</div>
+                    <div>Арт дирекція</div>
+                    <div>Проектування простору</div>
+                    <div>Комплексна розробка проектної документації</div>
+                    <div>Комерційна візуалізація</div>
+                
                 </div>
             </main>
             <Footer />
