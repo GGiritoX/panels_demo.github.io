e80922c295a1944ce4e35dc041b761a5bd8f826a
Vit <ggiritox@gmail.com>
2024-05-17 11:54:24 +0300

    update fullscreen for mobiles, update contact styles

--- src/pages/Project1.jsx
+++ src/pages/Project1.jsx
@@ -16,7 +16,6 @@ export default function Project1() {
     useEffect(() => {
         const handleLoad = () => {
             createCarousel();
-            // createFullscreenView();
             let indicators = document.querySelector('.my-carousel-indicator-container');
             if (!window.matchMedia("(max-width: 1400px)").matches) {
                 // console.log('carousel created');
@@ -27,9 +26,7 @@ export default function Project1() {
         window.addEventListener('load', handleLoad);
         return () => {
             window.removeEventListener('load', handleLoad);
-            if (window.matchMedia("(max-width: 1400px)").matches) {
-                createFullscreenView();
-            }
+            createFullscreenView();
         };
     }, []);
 
